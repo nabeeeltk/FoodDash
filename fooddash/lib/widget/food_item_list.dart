891249fbda8ab0d25a6753762582c957f55a309b
nabeeeltk <nabeelmuhammed406@gmail.com>
2023-08-24 17:08:55 +0530

    commit

--- fooddash/lib/widget/food_item_list.dart
+++ fooddash/lib/widget/food_item_list.dart
@@ -7,7 +7,6 @@ import '../view/food_details_page/food_details.dart';
 class FoodItemList extends StatelessWidget {
   final _controller = Get.put(AddNewItemcontrller());
   FoodItemList({super.key});
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -18,41 +17,62 @@ class FoodItemList extends StatelessWidget {
               itemBuilder: (context, index) {
                 var item = _controller.menuItems[index];
                 log(_controller.menuItems.length.toString());
-                return ListTile(
-                  horizontalTitleGap: 5,
-                  title: Text(
-                     item.itemname.toString(),
-                    style: const TextStyle(
-                        fontSize: 18,
-                        height: 0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.orange),
-                  ),
-                  subtitle: Text(
-                    item.itemDescription.toString(),
-                    style: const TextStyle(fontSize: 15, color: Colors.white),
-                  ),
-                  leading: Container(
-                    height: 100,
-                    width: 80,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: NetworkImage(item.imageUrl.toString()),
-                            fit: BoxFit.cover)),
-                  ),
+                return InkWell(
                   onTap: () {
-                    Get.to( FoodDetailsPage(pitem: item,));
+                    Get.to(FoodDetailsPage(
+                      pitem: item,
+                    ));
                   },
-                  trailing: Column(
-                    children: [
-                      Text(
-                       "₹ ${item.itemPrice.toString()}",
-                        style: const TextStyle(
-                            color: Colors.green,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
+                  child: Card(
+                    elevation: 5,
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                height: 100,
+                                width: 100,
+                                decoration: BoxDecoration(
+                                    color: Colors.blueGrey,
+                                    image: DecorationImage(
+                                        image: NetworkImage(
+                                            item.imageUrl.toString()),
+                                        fit: BoxFit.cover)),
+                              ),
+                            ),
+                            Column(
+                              children: [
+                                Text(
+                                  item.itemname.toString(),
+                                  style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.orange),
+                                ),
+                                Text(
+                                  item.itemDescription.toString(),
+                                  style: const TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text("₹ ${item.itemPrice.toString()}",
+                                  style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.green)),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
@@ -60,7 +80,6 @@ class FoodItemList extends StatelessWidget {
                 return const Divider(thickness: 0, color: Colors.black);
               },
               itemCount: _controller.menuItems.length);
-        }
-        );
+        });
   }
 }
